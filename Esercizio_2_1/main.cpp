d5055bf3bb2ecea6c0ad0d9a8ed76ca4a5e567a8
Filippo Graziani <otmw.electronicmusic@gmail.com>
2023-08-22 20:29:26 +0200

    Aggiunta documentazione per il primo esercizio
    Docs: aggiunti commenti Doxygen per esercizio

--- Esercizio_2_1/main.cpp
+++ Esercizio_2_1/main.cpp
@@ -1,10 +1,23 @@
+/**
+ * @file    main.cpp
+ * @author  Filippo Graziani
+ * @date    22/08/2023
+ * @brief   Simple program to test the classic Hello world in C++.
+ */
+
 #include <iostream>
 
+/**
+ * @brief   Main function
+ * @par     Description
+ * The program shows the line "Hello world!" on your monitor.
+ * @return  Always 0 (success).
+ */
 int
 main ()
 {
     std::cout << "Hello world!\n";
     return 0;
-}
+}   /* main() */
 
 /*** End of file ***/
