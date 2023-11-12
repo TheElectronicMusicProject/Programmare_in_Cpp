dd97d206f136cb4312f52fc1a7b5e8445e217b52
Filippo Graziani <otmw.electronicmusic@gmail.com>
2023-11-12 15:40:47 +0100

    Esercizi capitolo 17
    Feat: aggiunto l'esercizio 17.2

--- Esercizio_17_2/main.cpp
+++ Esercizio_17_2/main.cpp
@@ -1,7 +1,7 @@
 /**
  * @file    main.cpp
  * @author  Filippo Graziani
- * @date    11/11/2023
+ * @date    12/11/2023
  * @brief   The program reads a file containing a vector of numbers and sums
  *          its contents.
  */
@@ -10,8 +10,11 @@
 #include <fstream>
 #include <chrono>
 
-static inline long int sum_elem(const long int * const p_array,
-                                unsigned int size);
+static int matrix_product(const int * p_matrix1, int row1, int col1,
+                          const int * p_matrix2, int row2, int col2,
+                          int * p_matrix_out, int rowout, int colout);
+static void print_matrix(const int * p_matrix, unsigned int row,
+                         unsigned int col);
 
 /**
  * @brief   Main function
@@ -24,70 +27,66 @@ static inline long int sum_elem(const long int * const p_array,
 int
 main ()
 {
-    std::ifstream h_in_file;
-    h_in_file.open("input.txt");
 
-    if (true == h_in_file.fail())
+    std::ifstream h_file_mat1("matrix1.dat");
+
+    if (h_file_mat1.fail())
     {
-        std::cerr << "Impossible to open input.txt\n";
+        std::cout << "Error: cannot open matrix1.dat\n";
         exit(EXIT_FAILURE);
     }
 
-    unsigned int n_elems(0);
-    long int temp(0);
+    std::ifstream h_file_mat2("matrix2.dat");
 
-    while (h_in_file >> temp)
+    if (h_file_mat2.fail())
     {
-        ++n_elems;
+        std::cout << "Error: cannot open matrix2.dat\n";
+        exit(EXIT_FAILURE);
     }
 
-    // Clears EOF flag (it's a failing state), and request a seek to the
-    // beginning of the file.
-    //
-    h_in_file.clear();
-    h_in_file.seekg(0, std::ios::beg);
+    int temp(0);
+    int row_mat1(0);
+    int col_mat1(0);
 
-    long int array[n_elems]{0};
-    unsigned int idx(0);
+    h_file_mat1 >> row_mat1;
+    h_file_mat1 >> col_mat1;
 
-    while (h_in_file >> temp)
-    {
-        *(array + idx) = temp;
-        ++idx;
-    }
+    int mat1[2][3] = {{1, 0, 2}, {0, 3, -1}};
+    int mat2[3][2] = {{4, 1}, {-2, 2}, {0, 3}};
+    int mat_prod[2][2] = {0};
+    int ret(0);
 
-    std::cout << "There are " << n_elems << " elements" << '\n';
+    print_matrix(mat1[0], 2, 3);
+    print_matrix(mat2[0], 3, 2);
 
     // I could write auto instead of std::chrono::system_clock::time_point.
     //
     const std::chrono::steady_clock::time_point time_start =
                                             std::chrono::steady_clock::now();
-    
-    long int sum = sum_elem(array, n_elems);
+
+    ret = matrix_product(&mat1[0][0], 2, 3, &mat2[0][0], 3, 2,
+                         &mat_prod[0][0], 2, 2);
 
     const std::chrono::steady_clock::time_point time_end =
                                             std::chrono::steady_clock::now();
-
-
-    std::ofstream h_out_file;
-    h_out_file.open("output.txt", std::ios::out);
-
-    if (true == h_out_file.fail())
-    {
-        std::cerr << "Impossible to open output.txt\n";
-        exit(EXIT_FAILURE);
-    }
-
-    h_out_file << "There are: " << n_elems << " elements and their sum is "
-               << sum << std::endl;
-
-    h_in_file.close();
-    h_out_file.close();
 
     // Print the required execution time.
     //
     const std::chrono::duration<double> elapsed_seconds{time_end - time_start};
     std::cout << "Time required: " << elapsed_seconds.count() << " seconds\n";
+
+    if (0 == ret)
+    {
+        print_matrix(mat_prod[0], 2, 2);
+    }
+
+    std::ofstream h_file_out("result.dat");
+
+    if (h_file_out.fail())
+    {
+        std::cout << "Error: cannot open result.dat\n";
+        exit(EXIT_FAILURE);
+    }
     
     return 0;
 }   /* main() */
@@ -98,20 +97,94 @@ main ()
  * @param[in]   size    Size of the array (its number of elements).
  * @return      The sum of all the elements.
  */
-static inline long int
-sum_elem (const long int * p_array,
-          unsigned int size)
+static int
+matrix_product (const int * p_matrix1, int row1, int col1,
+                const int * p_matrix2, int row2, int col2,
+                int * p_matrix_out, int rowout, int colout)
 {
-    unsigned int idx(0);
-    long int sum(0);
+    int ret = 0;
 
-    for (idx = 0; idx < size; ++idx)
+    if ((rowout != row1) || (colout != col2))
     {
-        sum += *(p_array + idx);
+        ret = -1;
+    }
+    else if (col1 != row2)
+    {
+        ret = -2;
+    }
+    else if ((0 == row1) || (0 == col1) || (0 == row2) || (0 == col2) ||
+             (0 == rowout) || (0 == colout))
+    {
+        ret = -3;
+    }
+    else if ((NULL == p_matrix1) || (NULL == p_matrix2) ||
+             (NULL == p_matrix_out))
+    {
+        ret = -4;
+    }
+    else
+    {
+        int idx1(0);
+        int idx2(0);
+        int idx3(0);
+        
+        for (idx1 = 0; idx1 < row1; ++idx1)
+        {
+            for (idx2 = 0; idx2 < col2; ++idx2)
+            {
+                *(p_matrix_out + col2 * idx1 + idx2) = 0;
+
+                for (idx3 = 0; idx3 < col1; ++idx3)
+                {
+                    *(p_matrix_out + col2 * idx1 + idx2) +=
+                                            *(p_matrix1 + col1 * idx1 + idx3) *
+                                            *(p_matrix2 + col2 * idx3 + idx2);
+                }
+            }
+        }
+
+        ret = 0;
     }
 
-    return sum;
-}   /* sum_elem() */
+    return ret;
+}   /* matrix_product() */
+
+static void
+print_matrix (const int * p_matrix,
+              unsigned int row,
+              unsigned int col)
+{
+    if (NULL == p_matrix)
+    {
+        std::cerr << "Error: NULL pointer\n";
+    }
+    else if (0 == row)
+    {
+        std::cerr << "Error: can't process 0 rows\n";
+    }
+    else if (0 == col)
+    {
+        std::cerr << "Error: can't process 0 columns\n";
+    }
+    else
+    {
+        std::cout << "The matrix has " << row << " rows and "
+                  << col << " columns\n";
+
+        unsigned int idx1(0);
+        unsigned int idx2(0);
+
+        for (idx1 = 0; idx1 < row; ++idx1)
+        {
+            for (idx2 = 0; idx2 < col; ++idx2)
+            {
+                std::cout << *(p_matrix + col * idx1 + idx2) << '\t';
+            }
+
+            std::cout << std::endl;
+        }
+    }
+}   /* print_matrix() */
 
 
 /*** End of file ***/
